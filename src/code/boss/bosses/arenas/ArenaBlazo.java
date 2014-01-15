1532575ddd36edb8f314d88fee0f6f0ba6daca4d
rasmusrune <rasmusth@outlook.dk>
2014-01-15 10:36:09 +0100

    fixed a tiny bug

--- src/code/boss/bosses/arenas/ArenaBlazo.java
+++ src/code/boss/bosses/arenas/ArenaBlazo.java
@@ -3,13 +3,13 @@ package code.boss.bosses.arenas;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
-import code.boss.bosses.BossBeast;
+import code.boss.bosses.BossBlazo;
 import code.boss.main.main;
 
 public class ArenaBlazo extends Arena{
 	
 	public ArenaBlazo(main instance) {
-		super(instance, new BossBeast(instance), new Location(Bukkit.getWorld("BOSS"), -393, 62.5, -8));
+		super(instance, new BossBlazo(instance), new Location(Bukkit.getWorld("BOSS"), -393, 62.5, -8));
 	}
 
 }
